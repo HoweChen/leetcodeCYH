aac155c1805f321d698707c57046a94b603e3f60
SkyFather <cyh278889789@gmail.com>
2016-11-09 16:06:29 +0800

    update

--- 383. Ransom Note/383. Ransom Note.cpp
+++ 383. Ransom Note/383. Ransom Note.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        size_t pos = 0;
         for(size_t i = 0; i< ransomNote.size();++i){
             size_t pos = magazine.find(ransomNote[i], 0);
             if(pos != std::string::npos){
