58f10120f34ea6fe69d1ef8c377f8b4a57fe8b47
HoweChen <yuhao.howe.chen@gmail.com>
2018-11-15 23:07:04 +0800

    update 700

--- 700. Search in a Binary Search Tree/main.py
+++ 700. Search in a Binary Search Tree/main.py
@@ -12,9 +12,27 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+
+        # recursive way
         if not root:
             return 0
         else:
             left_depth = self.maxDepth(root.left)
             right_depth = self.maxDepth(root.right)
             return max(left_depth, right_depth)+1
+
+        # iterative way
+
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, root.left))
+                stack.append((current_depth + 1, root.right))
+
+        return depth
