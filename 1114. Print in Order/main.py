549f24b5274d1e38de6dc31da75f87972b31c7dd
HoweChen <yuhao.howe.chen@gmail.com>
2019-08-04 17:04:50 +0800

    add 1114

---
+++ 1114. Print in Order/main.py
@@ -0,0 +1,21 @@
+from threading import Barrier
+
+
+class Foo:
+    def __init__(self):
+        self.first_barrier = Barrier(3)
+        self.second_barrier = Barrier(2)
+
+    def first(self, printFirst):
+        printFirst()
+        self.first_barrier.wait()
+
+    def second(self, printSecond):
+        self.first_barrier.wait()
+        printSecond()
+        self.second_barrier.wait()
+
+    def third(self, printThird):
+        self.first_barrier.wait()
+        self.second_barrier.wait()
+        printThird()
